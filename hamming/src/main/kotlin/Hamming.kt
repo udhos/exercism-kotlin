2d73db317ad23cac10a3d517765d9a288f404c77
udhos <udhos>
2021-03-01 18:53:50 -0300

    hamming

---
+++ hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,19 @@
+object Hamming {
+
+    fun compute(leftStrand: String, rightStrand: String): Int {
+
+        if (leftStrand.length != rightStrand.length) {
+            throw IllegalArgumentException("left and right strands must be of equal length")
+        }
+
+        var distance = 0
+
+        for (i in 0 until leftStrand.length) {
+            if (leftStrand.get(i) != rightStrand.get(i)) {
+                distance++
+            }
+        }
+
+        return distance
+    }
+}
