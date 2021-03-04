6cf4bad51b2e692ece16000ffca13572a82888ab
udhos <udhos>
2021-03-04 19:57:25 -0300

    Use require, zip, count, filter.

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -2,18 +2,22 @@ object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
 
+        /*
         if (leftStrand.length != rightStrand.length) {
             throw IllegalArgumentException("left and right strands must be of equal length")
         }
+        */
+        require (leftStrand.length == rightStrand.length) { "left and right strands must be of equal length" }
 
+        /*
         var distance = 0
-
         for (i in 0 until leftStrand.length) {
             if (leftStrand.get(i) != rightStrand.get(i)) {
                 distance++
             }
         }
-
         return distance
+        */
+        return leftStrand.zip(rightStrand).count { it.first != it.second }
     }
 }
